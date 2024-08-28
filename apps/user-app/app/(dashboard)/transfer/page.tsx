1f5361dd6fff9961e954c5f5bb2b5df67c0aae0b
Raghu Anand <raghuaanand@gmail.com>
2024-08-28 07:01:08 +0530

    final stages done

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -6,7 +6,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../../lib/auth";
 
 async function getBalance() {
-    const session = await getServerSession(authOptions);
+    const session = await getServerSession(authOptions); // an asynchronous function that returns a promise. it is used to retrieve the session info from the server
     const balance = await prisma.balance.findFirst({
         where: {
             userId: Number(session?.user?.id)
